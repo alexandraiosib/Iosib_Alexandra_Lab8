cde69b252d7555217d770d35a9f707f33b37dd79
alexandraiosib <alexandra.iosib@gmail.com>
2021-11-22 14:05:26 +0200

    Add project files.

---
+++ Data/Iosib_Alexandra_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Iosib_Alexandra_Lab8.Models;
+
+namespace Iosib_Alexandra_Lab8.Data
+{
+    public class Iosib_Alexandra_Lab8Context : DbContext
+    {
+        public Iosib_Alexandra_Lab8Context (DbContextOptions<Iosib_Alexandra_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Iosib_Alexandra_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Iosib_Alexandra_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Iosib_Alexandra_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Iosib_Alexandra_Lab8.Data;
+using Iosib_Alexandra_Lab8.Models;
+
+namespace Iosib_Alexandra_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Iosib_Alexandra_Lab8.Data.Iosib_Alexandra_Lab8Context _context;
+
+        public IndexModel(Iosib_Alexandra_Lab8.Data.Iosib_Alexandra_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
